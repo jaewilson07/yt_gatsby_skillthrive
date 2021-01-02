dad9477085b07d1b118cee58ab805f1f5d10cc4a
Jae Wilson <jaewilson07@gmail.com>
2021-01-02 01:05:08 +0100

    add content card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from "react"
-import { FeatureImage, Container } from "../components"
+import { FeatureImage, Container , Content, ContentCard} from "../components"
 
 const IndexPage = () => (
   <Container>
     <FeatureImage />
+    <Content>
+      <ContentCard
+        date="1/1/2015"
+        title="test me"
+        excerpt="what a great artyicle"
+        slug="/test"
+      />
+    </Content>
   </Container>
 )
 
