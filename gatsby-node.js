cdc22ee5379b6586b1d5bde14d8ed08ddeea2644
Jae Wilson <jaewilson07@gmail.com>
2021-01-14 12:37:29 +0100

    updated gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,9 +4,40 @@
  */
 
 const path = require("path")
-const { createFilePath } = require("gatsby-source-filesystem")
 
-const { BASE_URL, POST_PER_PAGE } = require("./src/config")
+// const { createFilePath } = require("gatsby-source-filesystem")
+
+const {
+  BASE_URL,
+  POST_PER_PAGE,
+  GATSBY_TRAILINGSLASH,
+} = require("./src/config")
+
+const generateSlug = (parent, node) => {
+  let value = node.id
+
+  if (parent) {
+    value = parent.relativePath.replace(parent.ext, "")
+  }
+  if (node.frontmatter && node.frontmatter.slug) {
+    value = node.frontmatter.slug
+  }
+
+  if (value === "index") {
+    value = ""
+  }
+
+  // switch (node_type) {
+  //   case "notion":
+  //     const t_slug = url ? `/blog/${url}` : `/blog/${slug}`
+  //     console.log(node_type, t_slug, "the original slug: ", slug)
+  //     return t_slug
+
+  //   default:
+  return GATSBY_TRAILINGSLASH ? `${value}/` : value
+
+  // }
+}
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const result = await graphql(`
@@ -14,8 +45,8 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
-            id
-            frontmatter {
+            fields {
+              id
               slug
             }
           }
@@ -30,8 +61,8 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   }
 
   //create paginated pages for posts
-  const posts = result.data.allMdx.edges
-  const numPages = Math.ceil(posts.length / POST_PER_PAGE)
+  const edges = result.data.allMdx.edges
+  const numPages = Math.ceil(edges.length / POST_PER_PAGE)
 
   Array.from({ length: numPages }).forEach((_, index) => {
     actions.createPage({
@@ -47,13 +78,14 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   })
 
   // create single posts
-  posts.forEach((post, index) => {
-    const slug = `${BASE_URL}${post.node.frontmatter.slug}`
-    const id = post.node.id
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+  edges.forEach(({ node }, index) => {
+    console.log(Object.keys(node))
+    const { id, slug } = node.fields
 
-    console.log(slug)
+    console.log(id, slug)
+
+    const previous = index === edges.length - 1 ? null : edges[index + 1].node
+    const next = index === 0 ? null : edges[index - 1].node
 
     actions.createPage({
       path: slug,
@@ -69,12 +101,23 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  if (node.internal.type === `Mdx`) {
-    const value = createFilePath({ node, getNode })
-    actions.createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+  const { createNodeField } = actions
+
+  const parent = getNode(node.parent)
+
+  const slug = generateSlug(parent, node)
+
+  console.log(slug)
+
+  createNodeField({
+    name: "id",
+    node,
+    value: node.id,
+  })
+
+  createNodeField({
+    name: `slug`,
+    node,
+    value: slug,
+  })
 }
